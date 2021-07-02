4878fa532917713af66e7c2b8998e5f393364598
Mark Jensen <mark.jensen1@gmail.com>
2021-07-02 17:24:13 -0600

    Update the project with a d3 chart

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -8,7 +8,9 @@ app = FastAPI()
 
 origins = [
         "http://localhost:3000",
-        "localhost:3000"
+        "localhost:3000",
+        "http://localhost:3001",
+        "localhost:3001"
 ]
 
 
--- frontend/src/components/books.component.js
+++ frontend/src/components/books.component.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
+import * as d3 from 'd3';
+import MultilineChart from './MultilineChart';
+import schc from "../SCHC.json";
+import vcit from "../VCIT.json";
+import portfolio from "../PORTFOLIO.json";
 
+const portfolioData = { name: "Portfolio", color: "#ffffff", items: portfolio.map((d) => ({ ...d, date: new Date(d.date) })) };
+const schcData = { name: "SCHC", color: "#d53e4f", items: schc.map((d) => ({ ...d, date: new Date(d.date) }))};
+const vcitData = { name: "VCIT", color: "#5e4fa2", items: vcit.map((d) => ({ ...d, date: new Date(d.date) })) };
+const dimensions = {
+  width: 600,
+  height: 300,
+  margin: { top: 30, right: 30, bottom: 30, left: 60 }
+};
 class BooksComponent extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			error: null,
 			isLoaded: false,
-			books: []
+			books: [],
+            weekAvg: {}
 		}
 	}
 
 	componentDidMount() {
+
 		var url = "http://localhost:8080/api/books/list"
 		fetch(url)
 			.then(res => res.json())
@@ -29,7 +45,15 @@ class BooksComponent extends React.Component {
 					});
 				}
 			)
-	}
+		
+		axios.get(`http://localhost:8080/api/fitness/period?end=1-1-2021&start=12-25-2000`)
+            .then(res => {
+                const sortedItems = res.data.sort((a, b) => (a.fitnessdate > b.fitnessdate) ? 1 : -1);
+                this.setState({
+                    weekAvg: { name: "Fitness", color: "#08c96b", items: sortedItems.map((d) => ({ ...d, date: new Date(d.fitnessdate) })) }
+                });
+        })
+    }
 
 	render() {
 		const { error, isLoaded, books } = this.state;
@@ -39,15 +63,22 @@ class BooksComponent extends React.Component {
 		} else if (!isLoaded) {
 			return <div>Loading...</div>
 		} else {
-			return <div><ul>
-				{books.map((book, idx) => (
-					<li key={idx}>
-						{book}
-					</li>
-				))}
-			</ul>
-			<button variant="contained">Do Things!</button>
-			</div>
+			return (
+                <div>
+                    <ul>
+                        {books.map((book, idx) => (
+                            <li key={idx}>
+                                {book}
+                            </li>
+                        ))}
+                    </ul>
+                    <MultilineChart
+                        data={[this.state.weekAvg]}
+                        dimensions={dimensions}
+                    />
+                    <button variant="contained">Do Things!</button>
+                </div>
+            )
 		}
 
 	}
